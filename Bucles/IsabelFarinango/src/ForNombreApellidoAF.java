b15c4376fc6b7fb92181ac5297ef47754d1046fa
y-iss <alegria.farinango@epn.edu.ec>
2024-05-12 00:23:13 -0500

    Ahora ciclo while

--- Bucles/IsabelFarinango/src/ForNombreApellidoAF.java
+++ Bucles/IsabelFarinango/src/ForNombreApellidoAF.java
@@ -1,27 +1,27 @@
 public class ForNombreApellidoAF {
-    public void forNombreApellido (int nivelEsperado, String signo) {
+    public void forNombreApellido(int nivelEsperado, String signo) {
         System.out.println();
         System.out.println("Las iniciales A ; F con FOR");
         for (int fil = 1; fil <= nivelEsperado; fil++) {
-            for (int col = 1; col <= nivelEsperado; col++){
+            for (int col = 1; col <= nivelEsperado; col++) {
                 if (col == 1 || fil == nivelEsperado) {
                     System.out.print(signo + " ");
                 } else {
                     System.out.print(" ");
                 }
-            } 
+            }
             System.out.print(" ");
-            for (int col = 1; col <= nivelEsperado; col++){
-                if((fil == 1 || fil == nivelEsperado / 2 + 1 ) && col <= nivelEsperado) {
+            for (int col = 1; col <= nivelEsperado; col++) {
+                if ((fil == 1 || fil == nivelEsperado / 2 + 1) && col <= nivelEsperado) {
                     System.out.print(signo + " ");
                 } else {
-                    if (fil ! = 1 && fil ! = nivelEsperado / 2 + 1 && (col == 1 || col == nivelEsperado)){
+                    if (fil != 1 && fil != nivelEsperado / 2 + 1 && (col == 1 || col == nivelEsperado)) {
                         System.out.print(signo + " ");
                     } else {
                         System.out.print(" ");
                     }
                 }
-            } 
+            }
             System.out.println();
         }
         System.out.println();
