a7c187fcac4168b538d3c0aeaaf89c1bbf6fcff3
y-iss <alegria.farinango@epn.edu.ec>
2024-05-26 17:30:15 -0500

    cambio

--- Retos_patmic/Retos_Fase1/src/App.java
+++ Retos_patmic/Retos_Fase1/src/App.java
@@ -8,54 +8,23 @@ public class App {
         System.out.println("Elija la serie que desea generar:");
         System.out.println("1. S12");
         System.out.println("2. SC1");
-       
+
+
         int serieOpcion = scanner.nextInt();
 
         System.out.print("Ingrese el numero de terminos: ");
         int terminos = scanner.nextInt();
 
-        System.out.println("Elija el tipo de bucle:");
-        System.out.println("1. For");
-        System.out.println("2. While");
-        System.out.println("3. DoWhile");
-
-        int bucleOpcion = scanner.nextInt();
-
         switch (serieOpcion) {
             case 1:
-                switch (bucleOpcion) {
-                    case 1:
-                        fa.FA_S12_For(terminos);
-                        break;
-                    case 2:
-                        fa.FA_S12_While(terminos);
-                        break;
-                    case 3:
-                        fa.FA_S12_DoWhile(terminos);
-                        break;
-                        default:
-                        System.out.println("Opcion de bucle no valida.");
-                        break;
-                }
+                fa.FA_S12(terminos);
                 break;
-            case 2: 
-                switch(bucleOpcion){
-                    case 1: 
-                        fa.FA_SC1_For(terminos);
-                        break; 
-                    case 2:
-                        fa.FA_SC1_While(terminos);
-                        break; 
-                    case 3: 
-                        fa.FA_SC1_DoWhile(terminos);
-                        break; 
-                        default: 
-                        System.out.println("Opcion de bucle no valida");
-                        break; 
-                }
-                
-        scanner.close();
+            case 2:
+                fa.FA_SC1(terminos);
+                break;
         }
+
+        scanner.close();
     }
 }
 
--- Retos_patmic/Retos_Fase1/src/FarinangoAlegria.java
+++ Retos_patmic/Retos_Fase1/src/FarinangoAlegria.java
@@ -3,7 +3,7 @@ public class FarinangoAlegria {
     private String FANombre;
 
     // metodos S12
-    public void FA_S12_For(int terminos) {
+    public void FA_S12(int terminos) {
         int valor = 2;
         for (int i = 1; i <= terminos; i++) {
             System.out.print(valor + " ");
@@ -11,53 +11,14 @@ public class FarinangoAlegria {
         }
         System.out.println();
     }
-    public void FA_S12_While(int terminos){
-        int valor = 2; 
-        int i = 1; 
-        while (i <= terminos){
-            System.out.print(valor + " ");
-            valor += (i + 1) * 2; 
-            i++; 
-        }
-        System.out.println();
-    }
-    public void FA_S12_DoWhile(int terminos){
-        int valor = 2; 
-        int i = 1; 
-            do {
-                System.out.print(valor + " ");
-                valor += (i + 1) * 2; 
-                i++; 
-            } while (i <= terminos); 
-            System.out.println();
-    }
-    // metodos SC
-    public void FA_SC1_For(int terminos){
-        char caracter = '+'; 
-        for (int i = 0; i < terminos; i++){
-            System.out.print(caracter + " ");
-            caracter = (caracter == '+') ? '-' : '+'; 
-        }
-        System.out.println();
-    }
-    public void FA_SC1_While(int terminos){
+
+    // metodos SC1
+    public void FA_SC1(int terminos) {
         char caracter = '+';
-        int i = 0; 
-        while (i<terminos) {
+        for (int i = 0; i < terminos; i++) {
             System.out.print(caracter + " ");
-            caracter = (caracter == '+') ? '-' : '+'; 
-            i++; 
+            caracter = (caracter == '+') ? '-' : '+';
         }
-        System.out.println();
-    }
-    public void FA_SC1_DoWhile(int terminos){
-        char caracter = '+'; 
-        int i = 0; 
-        do {
-            System.out.print(caracter + " ");
-            caracter = (caracter == '+') ? '-' : '+'; 
-            i++; 
-        } while (i<terminos); 
         System.out.println();
     }
 }
