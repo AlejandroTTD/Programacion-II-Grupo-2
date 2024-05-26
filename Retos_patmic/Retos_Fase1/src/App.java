82948c15b64a0d5705b5bd8406d32ef52fab0e4f
y-iss <alegria.farinango@epn.edu.ec>
2024-05-25 22:41:30 -0500

    serie 12

--- Retos_patmic/Retos_Fase1/src/App.java
+++ Retos_patmic/Retos_Fase1/src/App.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, y-iss!");
+    public static void main(String[] args) {
+        FarinangoAlegria serie = new FarinangoAlegria();
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("Seleccione la serie que desea generar:");
+            System.out.println("1. Serie Numérica S12");
+            option = scanner.nextInt();
+
+            if (option < 1 || option > 10) {
+                if (option == 11) {
+                    System.out.println("Saliendo...");
+                    break;
+                }
+                System.out.println("Opción no válida. Intente de nuevo.");
+                continue;
+            }
+
+            System.out.print("Ingrese el número de términos/caracteres: ");
+            int numElementos = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    generarSerieS12(serie, numElementos);
+                    break;
+
+            }
+
+            System.out.println();
+
+        } while (option != 11);
+
+        scanner.close();
     }
+
+    private static void generarSerieS12(FarinangoAlegria serie, int numElementos) {
+        serie.FAS1S12(numElementos);
+    }
+
 }
+
---
+++ Retos_patmic/Retos_Fase1/src/FarinangoAlegria.java
@@ -0,0 +1,13 @@
+public class FarinangoAlegria {
+
+    //  S12
+     public void FAS1S12(int terms) {
+        int FANum = 2;
+        for (int i = 1; i <= terms; i++) {
+            System.out.print(FANum + " ");
+            FANum += i * 2;
+        }
+        System.out.println();
+    }
+
+}
