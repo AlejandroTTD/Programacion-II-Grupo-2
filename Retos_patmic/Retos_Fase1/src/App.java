834f2c9efebc00f3a6002a72beb968868f1cb8ed
y-iss <alegria.farinango@epn.edu.ec>
2024-05-26 17:48:30 -0500

    hasta la SC6

--- Retos_patmic/Retos_Fase1/src/App.java
+++ Retos_patmic/Retos_Fase1/src/App.java
@@ -8,6 +8,10 @@ public class App {
         System.out.println("Elija la serie que desea generar:");
         System.out.println("1. S12");
         System.out.println("2. SC1");
+        System.out.println("3. SC2");
+        System.out.println("4. SC3");
+        System.out.println("5. SC4");
+        System.out.println("6. SC5");
 
 
         int serieOpcion = scanner.nextInt();
@@ -21,6 +25,15 @@ public class App {
                 break;
             case 2:
                 fa.FA_SC1(terminos);
+                break;
+            case 3:
+                fa.FA_SC2(terminos);
+                break;
+            case 4:
+                fa.FA_SC3(terminos);
+                break;
+            case 5:
+                fa.FA_SC4(terminos);
                 break;
         }
 
--- Retos_patmic/Retos_Fase1/src/FarinangoAlegria.java
+++ Retos_patmic/Retos_Fase1/src/FarinangoAlegria.java
@@ -12,7 +12,7 @@ public class FarinangoAlegria {
         System.out.println();
     }
 
-    // metodos SC1
+    // metodos SC
     public void FA_SC1(int terminos) {
         char caracter = '+';
         for (int i = 0; i < terminos; i++) {
@@ -21,4 +21,61 @@ public class FarinangoAlegria {
         }
         System.out.println();
     }
+
+    public void FA_SC2(int terminos){
+        int length = 1; 
+        for(int i = 0; i<terminos; i++){
+            for(int j = 0; j<length; j++){
+                System.out.print('+');
+            }
+            System.out.print(" ");
+            length += i + 1; 
+        }
+        System.out.println();
+    }
+
+    public void FA_SC3(int terminos){
+        int length = 2; 
+        for(int i = 0; i < terminos; i++){
+            for(int j = 0; j < length; j++){
+                System.out.print('+');
+            }
+            System.out.print(" ");
+            length += 2 * i +1; 
+        }
+        System.out.println();
+    }
+
+    public void FA_SC4(int terminos) {
+        char[] simbolos = {'+', '-', '*', '/'};
+        for (int i = 0; i < terminos; i++) {
+            System.out.print(simbolos[i % simbolos.length] + " ");
+        }
+        System.out.println();
+    }
+
+    public void FA_SC5(int terminos) {
+        char[] simbolos = {'\\', '|', '/', '-'};
+        for (int i = 0; i < terminos; i++) {
+            System.out.print(simbolos[i % simbolos.length] + " ");
+        }
+        System.out.println();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
