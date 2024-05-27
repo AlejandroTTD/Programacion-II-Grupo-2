7eca75cf22346ce0822dfc820157235b41f8d4dc
y-iss <alegria.farinango@epn.edu.ec>
2024-05-26 20:38:45 -0500

    cambio

--- Retos_patmic/Retos_Fase1/src/App.java
+++ Retos_patmic/Retos_Fase1/src/App.java
@@ -34,58 +34,18 @@ public class App {
         int g2TerminosI = 0;
         
         System.out.println("Elija la serie que desea generar:");
-        System.out.println("1. S12");
-        System.out.println("2. SC1");
-        System.out.println("3. SC2");
-        System.out.println("4. SC3");
-        System.out.println("5. SC4");
-        System.out.println("6. SC5");
-        System.out.println("7. SC6");
-        System.out.println("8. SC7");
-        System.out.println("9. SC8");
-        System.out.println("10. SC9");
+        System.out.println("17. S12");
+        System.out.println("18. SC1");
+        System.out.println("19. SC2");
+        System.out.println("20. SC3");
+        System.out.println("21. SC4");
+        System.out.println("22. SC5");
+        System.out.println("23. SC6");
+        System.out.println("24. SC7");
+        System.out.println("25. SC8");
+        System.out.println("26. SC9");
 
-        int serieOpcion = scanner.nextInt();
-
-        System.out.print("Ingrese el numero de terminos: ");
-        int terminos = scanner.nextInt();
-
-        switch (serieOpcion) {
-            case 1:
-                fa.FA_S12(terminos);
-                break;
-            case 2:
-                fa.FA_SC1(terminos);
-                break;
-            case 3:
-                fa.FA_SC2(terminos);
-                break;
-            case 4:
-                fa.FA_SC3(terminos);
-                break;
-            case 5:
-                fa.FA_SC4(terminos);
-                break;
-            case 6:
-                fa.FA_SC5(terminos);
-                break;
-            case 7:
-                fa.FA_SC6(terminos);
-                break;
-            case 8:
-                fa.FA_SC7(terminos);
-                break;
-            case 9:
-                fa.FA_SC8(terminos);
-                break;
-            case 10:
-                fa.FA_SC9(terminos);
-                break;
-            default:
-                System.out.println("Opción de serie no válida.");
-                break;
-        }
-
+       
         while (true) {
             System.out.print("Ingrese el numero de terminos: ");
             String g2Terminos = g2Sc.nextLine();
@@ -147,8 +107,38 @@ public class App {
             case 16:
                 fa.FA_SC4(g2TerminosI);
                 break;
+            case 17:
+                fa.FA_S12(g2TerminosI);
+                break;
+            case 18:
+                fa.FA_SC1(g2TerminosI);
+                break;
+            case 19:
+                fa.FA_SC2(g2TerminosI);
+                break;
+            case 20:
+                fa.FA_SC3(g2TerminosI);
+                break;
+            case 21:
+                fa.FA_SC4(g2TerminosI);
+                break;
+            case 22:
+                fa.FA_SC5(g2TerminosI);
+                break;
+            case 23:
+                fa.FA_SC6(g2TerminosI);
+                break;
+            case 24:
+                fa.FA_SC7(g2TerminosI);
+                break;
+            case 25:
+                fa.FA_SC8(g2TerminosI);
+                break;
+            case 26:
+                fa.FA_SC9(g2TerminosI);
+                break;
             default:
-                System.out.println("Opción no valida");
+                System.out.println("Opción de serie no válida.");
                 break;
         }
     }
