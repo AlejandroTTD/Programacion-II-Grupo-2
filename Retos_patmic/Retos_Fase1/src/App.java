577ea65c32f49ce2a924ea2432a3fef0f11518d7
AlejandroTTD <davidcuasquer21@gmail.com>
2024-05-26 19:50:54 -0500

    BugFix App

--- Retos_patmic/Retos_Fase1/src/App.java
+++ Retos_patmic/Retos_Fase1/src/App.java
@@ -5,7 +5,8 @@ public class App {
     public static void main(String[] args) {
         FarinangoAlegria fa = new FarinangoAlegria();
         CuasquerDavid da = new CuasquerDavid();
-        System.out.println("Elija la serie que desea generar:");
+
+        // Interfaz
         System.out.println("1. S1");
         System.out.println("2. S2");
         System.out.println("3. S3");
@@ -22,59 +23,88 @@ public class App {
         System.out.println("15. SC4");
         System.out.println("16. SC5");
 
-        int g2SerieOpcion = g2Sc.nextInt();
+        int g2SerieOpcionI = 0;
+        int g2TerminosI = 0;
+        
+        while (true) {
+            System.out.println("Elija la serie que desea generar:");
+            String g2SerieOpcion = g2Sc.nextLine();
+            try {
+                g2SerieOpcionI = Integer.parseInt(g2SerieOpcion);
+                if (g2SerieOpcionI >= 1 && g2SerieOpcionI <= 16 && g2SerieOpcionI != 11) {
+                    break; 
+                } else {
+                    System.out.println("Opción no válida. Intente de nuevo.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalido. Intente de nuevo.");
+            }
+        }
 
-        System.out.print("Ingrese el numero de terminos: ");
-        int g2Terminos = g2Sc.nextInt();
+        while (true) {
+            System.out.print("Ingrese el numero de terminos: ");
+            String g2Terminos = g2Sc.nextLine();
+            try {
+                g2TerminosI = Integer.parseInt(g2Terminos);
+                if (g2TerminosI > 0) {
+                    break; 
+                } else {
+                    System.out.println("El número de términos debe ser mayor que 0. Intente de nuevo.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalido. Intente de nuevo.");
+            }
+        }
 
-        switch (g2SerieOpcion) {
+        switch (g2SerieOpcionI) {
             case 1:
-                da.dcSN1(g2Terminos);
+                da.dcSN1(g2TerminosI);
                 break;
             case 2:
-                da.dcSN2(g2Terminos);
+                da.dcSN2(g2TerminosI);
                 break;
             case 3:
-                da.dcSN3(g2Terminos);
+                da.dcSN3(g2TerminosI);
                 break;
             case 4:
-                da.dcSN4(g2Terminos);
+                da.dcSN4(g2TerminosI);
                 break;
             case 5:
-                da.dcSN5(g2Terminos);
+                da.dcSN5(g2TerminosI);
                 break;
             case 6:
-                da.dcSN6(g2Terminos);
+                da.dcSN6(g2TerminosI);
                 break;
             case 7:
-                da.dcSN7(g2Terminos);
+                da.dcSN7(g2TerminosI);
                 break;
             case 8:
-                da.dcSN8(g2Terminos); 
+                da.dcSN8(g2TerminosI);
                 break;
             case 9:
-                da.dcSN9(g2Terminos);
+                da.dcSN9(g2TerminosI);
                 break;
             case 10:
-                da.dcSN10(g2Terminos);
+                da.dcSN10(g2TerminosI);
                 break;
             case 12:
-                fa.FA_S12(g2Terminos);
+                fa.FA_S12(g2TerminosI);
                 break;
             case 13:
-                fa.FA_SC1(g2Terminos);
+                fa.FA_SC1(g2TerminosI);
                 break;
             case 14:
-                fa.FA_SC2(g2Terminos);
+                fa.FA_SC2(g2TerminosI);
                 break;
             case 15:
-                fa.FA_SC3(g2Terminos);
+                fa.FA_SC3(g2TerminosI);
                 break;
             case 16:
-                fa.FA_SC4(g2Terminos);
+                fa.FA_SC4(g2TerminosI);
+                break;
+            default:
+                System.out.println("Opción no válida");
                 break;
         }
     }
 }
-
-
--- Retos_patmic/Retos_Fase1/src/CuasquerDavid.java
+++ Retos_patmic/Retos_Fase1/src/CuasquerDavid.java
@@ -14,7 +14,7 @@ public class CuasquerDavid {
     // SN1:  0 1 1 2 3 5 8 13 ...
     public void dcSN1(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
         System.out.println("Sucesion Numerica 1:");
         int dcA = 0;
         int dcB = 1;
@@ -29,7 +29,7 @@ public class CuasquerDavid {
     // Sn2:  1 0 3 0 5 0 7 0 9 ...
     public void dcSN2(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
         System.out.println("Sucesion Numerica 2:");
         for(int dcI = 1; dcI <= dcNum ; dcI++){
             if(dcI % 2 == 0){
@@ -43,7 +43,7 @@ public class CuasquerDavid {
     // Sn3:  0/1 1/3 1/5 2/7 3/9 5/11 8/13 13/15 ...
     public void dcSN3(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
         System.out.println("Sucesion Numerica 3:");
         int dcA = 0;
         int dcB = 1;
@@ -59,7 +59,7 @@ public class CuasquerDavid {
     // Sn4:  0/2 1/4 1/6 2/8 3/10 5/12 8/14 13/16 ...
     public void dcSN4(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
         System.out.println("Sucesion Numerica 4:");
         int dcA = 0;
         int dcB = 1;
@@ -75,7 +75,7 @@ public class CuasquerDavid {
     // Sn5:  2 3 5 7 11 13 17 19 23 29 31 ...
     public void dcSN5(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
 
         System.out.println("Sucesion Numerica 5:");
         int dcI = 1;
@@ -97,7 +97,7 @@ public class CuasquerDavid {
     // Sn6:  1 4 9 16 25 36 49 64 ...
     public void dcSN6(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
         System.out.println("Sucesion Numerica 6:");
         for(int dcI = 1; dcI <= dcNum ; dcI++){
             System.out.print((dcI*dcI)+" ");
@@ -106,7 +106,7 @@ public class CuasquerDavid {
     // Sn7: 1 4 7 10 13 16 19 22 25  ...
     public void dcSN7(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
         System.out.println("Sucesion Numerica 7:");
         for(int dcI = 0; dcI < dcNum ; dcI++){
             System.out.print((1+ (dcI*3))+" ");
@@ -115,7 +115,7 @@ public class CuasquerDavid {
     // Sn8: 3, 8, 13, 18, 23, 28, 33, 38, ...
     public void dcSN8(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
         System.out.println("Sucesion Numerica 8:");
         for(int dcI = 0; dcI < dcNum ; dcI++){
             System.out.print((3+(dcI*5))+", ");
@@ -124,7 +124,7 @@ public class CuasquerDavid {
     // Sn9: 2, 4, 8, 16, 32, 64, 128, 256, ...
     public void dcSN9(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
         System.out.println("Sucesion Numerica 9:");
         for(int dcI = 1; dcI <= dcNum; dcI++){
             System.out.print((int)Math.pow(2, dcI)+", ");
@@ -133,7 +133,7 @@ public class CuasquerDavid {
     // Sn10: 3, 9, 27, 81, 243, 729, 2187, ...
     public void dcSN10(int dcNum){
         System.out.println();
-        System.out.println("Ingrese el numero de cifras para la serie:");
+
         System.out.println("Sucesion Numerica 10:");
         for(int dcI =1; dcI <= dcNum; dcI++){
             System.out.print((int)Math.pow(3, dcI)+", ");
